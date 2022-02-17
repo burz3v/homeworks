1a4d596531d2f8a7dae724f487d7911a3945fc87
burz3v <97345685+burz3v@users.noreply.github.com>
2022-02-17 19:06:45 +0200

    Create exam_bonus.py

---
+++ exam_bonus.py
@@ -0,0 +1,25 @@
+#bonus question with *
+
+#list[]
+#הוא רשימה של משהו שאפשר לשנות אותה, להוסיף ולמחוק
+
+#tuple()
+#הוא רשימה שלא ניתן לבצע בה שום שינוי אלא רק לקחת מידה
+
+#set{}
+#הוא רשימה שכוללת בה רק פריטים מיוחדים ללא כפילויות
+
+#bonus question code with **
+
+a1 = '*'
+a2 = '***'
+a3 = '*****'
+a4 = input('Please enter the number ')
+
+print(a1.center(5))
+print(a2.center(5))
+print(a3.center(5))
+print(a4.center(5))
+print(a3.center(5))
+print(a2.center(5))
+print(a1.center(5))
